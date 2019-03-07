8ce827d8f8a8a5f2063096e7affd33b46f60b074
Drew <andrew.macquarrie@gmail.com>
2019-03-07 16:42:01 +0000

    updating to new Unity version; adding attention events manager that loads events from json and plays them at set times

---
+++ Assets/AttentionEventsController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class AttentionEventsController : MonoBehaviour {
+
+	public string PathToEventsFile;
+	private List<AttentionEvent> events;
+	int currentEventIndex;
+
+	[System.Serializable]
+	public enum EventType 
+	{
+		ARROW,
+		CLEAR
+	}
+
+	[System.Serializable]
+	public class AttentionEvent
+	{
+		public int startTime;
+		public int xPos;
+		public int yPos;
+		public EventType type;
+	}
+
+	[System.Serializable]
+	public class AllAttentionEventData
+	{
+		public List<AttentionEvent> events = new List<AttentionEvent>();
+	}
+
+	private List<AttentionEvent> LoadAttentionEvents(){
+		if (File.Exists (PathToEventsFile)) {
+			string dataAsJson = File.ReadAllText (PathToEventsFile);
+			var allEventsContainer = JsonUtility.FromJson<AllAttentionEventData> (dataAsJson);
+			return allEventsContainer.events;
+		}
+		Debug.Log ("Not able to load events - file path doesnt contain any events: " + PathToEventsFile);
+		return new List<AttentionEvent> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		events = LoadAttentionEvents ();
+
+		foreach (var e in events) {
+			Debug.Log (e.startTime);
+		}
+
+		currentEventIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentEventIndex >= events.Count) {
+			return;
+		}
+
+		var currentEvent = events [currentEventIndex];
+
+		if (Time.time > currentEvent.startTime) {
+			// do event thing
+			Debug.Log("doing event" + currentEvent.type);
+
+			currentEventIndex++;
+		}
+	}
+}
